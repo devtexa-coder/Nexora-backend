9fc0ff88064604fdb37782040228afd403349c61
Favour Ogbonda <ogbondafavour9@gmail.com>
2026-08-16 09:54:00 +0000

    fixed lobby refresh issue

--- src/realtime.ts
+++ src/realtime.ts
@@ -2,3 +2,4 @@ import { Server } from "socket.io";
 let io: Server | undefined;
 export const setRealtime = (server: Server) => { io = server; };
 export const emitRoom = (roomId: string, event: string, data: unknown) => io?.to(roomId).emit(event, data);
+export const emitLobbiesUpdated = () => io?.emit("lobbies:update");
